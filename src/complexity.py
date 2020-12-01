03cb0b18b34f080e8001bffeabec7f4e2ea76821
zyadbenameur <zyad.benameur@hec.ca>
2020-12-01 03:02:46 -0500

    refined version graham + update in time complexity analysis for plots

--- src/complexity.py
+++ src/complexity.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import brute as b
 import jarvis as j
 import graham as g
+import dandc as d
+
 
 def random_points(points, n):
     randomx = []
@@ -16,58 +18,99 @@ def random_points(points, n):
         points.append(Point(x, y))
 
 
-def time_complexity(k):
+def time_complexity(k_brute, k_divide, k):
     points = []
     times_brute = []
+    times_divide = []
     times_jarvis = []
     times_graham = []
     n = []
+    n_brute = []
+    n_divide = []
 
-    for i in range(5,k+5):
-        n.append(i)
+    for i in range(5,k_brute+5):
+        n_brute.append(i)
         random_points(points, i)
-        print(i-4)
+
+        print("------")
         # Brute
         tic = time.perf_counter()
         b.convex_hull(points, len(points))
         toc = time.perf_counter()
         times_brute.append(toc-tic)
+        print("------")
+        
+        print(i-4)
 
+    for i in range(5,k_divide+5):
+        n_divide.append(i)
+        random_points(points, i)
+
+        print("------")
+        # Divide and conquer
+        tic = time.perf_counter()
+        d.convex_hull(points,len(points),8)
+        toc = time.perf_counter()
+        times_divide.append(toc-tic)
+        print("------")
+        
+        print(i-4)
+
+    for i in range(5,k+5):
+        n.append(i)
+        random_points(points, i)
+
+        print("------")
         # Jarvis
         tic = time.perf_counter()
         j.convex_hull(points, len(points))
         toc = time.perf_counter()
         times_jarvis.append(toc-tic)
 
-        # Graham
+        # Graham O(nlogn)
         tic = time.perf_counter()
         g.convex_hull(points)
         toc = time.perf_counter()
         times_graham.append(toc-tic)
+        print("------")
+        
+        print(i-4)
+    
+    return n_brute, n_divide, n, times_brute, times_divide, times_jarvis, times_graham
 
-    return n, times_brute, times_jarvis, times_graham
 
 
+n_brute, n_divide, n, times_brute, times_divide, times_jarvis, times_graham = time_complexity(20,90,90)
 
-n, times_brute, times_jarvis, times_graham = time_complexity(20)
-
-
-# Plot brute algo
+# Plot brute algo ------------------
 fig, ax = plt.subplots()
-ax.plot(n,times_brute)
+ax.plot(n_brute,times_brute)
 
-ax.set(xlabel='Time required', ylabel='No. of elements',
+ax.set(xlabel='No. of elements', ylabel='Time required',
        title='Brute algo time complexity')
 ax.grid()
 
 fig.savefig("brute.png")
 plt.show()
 
-# Plot Jarvis algo
+
+# Plot Div&Conq algo ------------------
+fig, ax = plt.subplots()
+ax.plot(n_divide,times_divide)
+
+ax.set(xlabel='No. of elements', ylabel='Time required',
+       title='Divide and Conquer algo time complexity')
+ax.grid()
+
+fig.savefig("div&conq.png")
+plt.show()
+
+
+# Plot Jarvis algo ------------------
 fig, ax = plt.subplots()
 ax.plot(n,times_jarvis)
 
-ax.set(xlabel='Time required', ylabel='No. of elements',
+ax.set(xlabel='No. of elements', ylabel='Time required',
        title='Jarvis algo time complexity')
 ax.grid()
 
@@ -75,11 +118,11 @@ fig.savefig("jarvis.png")
 plt.show()
 
 
-# Plot Graham algo
+# Plot Graham algo ------------------
 fig, ax = plt.subplots()
 ax.plot(n,times_graham)
 
-ax.set(xlabel='Time required', ylabel='No. of elements',
+ax.set(xlabel='No. of elements', ylabel='Time required',
        title='Graham algo time complexity')
 ax.grid()
 
--- src/graham.py
+++ src/graham.py
@@ -38,6 +38,11 @@ def lowest_y(points):
 
     return points[minind], minind
 
+def euc_dist(p0,p1,p2):
+    d1=math.sqrt((p1.x-p0.x)**2 + (p1.y-p0.y)**2)
+    d2=math.sqrt((p2.x-p0.x)**2 + (p2.y-p0.y)**2)
+    return d1, d2
+
 """
 # 2. sort points by polar angle with P0, if several points have the same polar angle then only keep the farthest
 """
@@ -47,21 +52,14 @@ def polar_angle_sort(points,p0):
     sorted_points = []
     #calculate the polar angle with p0 for all points
     for i,point in enumerate(points):
-        vec_x=point.x-p0.x
-        vec_y=point.y-p0.y
-        #keeping index to find initial points
-        sorted_angle[i] = math.atan2(vec_y, vec_x) 
-        #if we find colinear vectors, we calculate lenght and choose farthest
-        if i >= 1:
-            if sorted_angle.get(i) == sorted_angle.get(i-1):
-                d1=math.sqrt(points[i-1].x**2 + points[i-1].y**2)
-                d2=math.sqrt(points[i].x**2 + points[i].y**2)
-                indice_p1=i-1
-                indice_p2=i
-                if d1>d2:
-                    sorted_angle.pop(indice_p2)
-                elif d1<=d2:
-                    sorted_angle.pop(indice_p1)
+        if point == p0:
+            pass
+        else:
+            vec_x=point.x-p0.x
+            vec_y=point.y-p0.y
+            #keeping index to find initial points
+            sorted_angle[i] = math.atan2(vec_y, vec_x) 
+
 
     #sort angles (gives a list)
     sorted_angle=sorted(sorted_angle.items(), key=lambda x: x[1], reverse=False)
@@ -73,6 +71,14 @@ def polar_angle_sort(points,p0):
     #sort points with sorted index
     for i,index in enumerate(sorted_index):
         sorted_points.append(points[index])
+
+    # We remove collinear points with p0 and keep only furthest
+    coll_points = []
+    for i in range(len(sorted_points) - 1):
+        d = p0.direction(sorted_points[i],sorted_points[i+1])
+        if d == 0:
+            coll_points.append(i)
+    sorted_points = [i for j, i in enumerate(sorted_points) if j not in coll_points]
 
     return sorted_points
 
@@ -102,29 +108,31 @@ def convex_hull(points):
         while True:
             direction = convex_hull[m-2].direction(convex_hull[m-1],points[i])
             if direction > 0:
-                break
+                break            
             else:
                 convex_hull.pop()
                 m -= 1
         convex_hull.append(points[i])
         m += 1
-    
-
+    '''
+    for i,p in enumerate(convex_hull):
+        print(p)
+    '''
     toc = time.perf_counter()
     print("Processing time for Graham scan convex hull: " + str(toc-tic) + " seconds")
     return convex_hull  # This needs to be an index of points
 
 
-#points = [] 
-#points.append(Point(0, 3)) 
-#points.append(Point(2, 2)) 
-#points.append(Point(1, 1)) 
-#points.append(Point(2, 1)) 
-#points.append(Point(3, 3)) 
-#points.append(Point(0, 0)) 
-#points.append(Point(3, 0))          
+points = [] 
+points.append(Point(0, 3)) 
+points.append(Point(2, 2)) 
+points.append(Point(1, 1)) 
+points.append(Point(2, 1)) 
+points.append(Point(3, 3)) 
+points.append(Point(0, 0)) 
+points.append(Point(3, 0))     
 
 
 
 
-#print(convex_hull(points))
+print(convex_hull(points))
